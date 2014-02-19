0b38c82f04b9dbd7746c552eb68c06d6371c2912
Atle Frenvik Sveen <atle.sveen@gmail.com>
2014-02-19 21:26:48 +0100

    add links, fix wrong statement (diabetes.js does not exist)

--- static/jslibornot.js
+++ static/jslibornot.js
@@ -3,7 +3,7 @@
 
     //+ Jonas Raoni Soares Silva
     //@ http://jsfromhell.com/array/shuffle [v1.0]
-    function shuffle(o){ //v1.0
+    function shuffle(o) { //v1.0
         for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
     }
@@ -33,19 +33,18 @@
         },
 
         getStats: function () {
-            var correct = this.where({correct: true}).length;
-            var num = this.length;
             var answers = this.map(function (question) {
                 return {
                     name: question.get('name'),
                     correct: question.get('correct'),
-                    isLib: question.get('isLib')
+                    isLib: question.get('isLib'),
+                    link: question.get("link")
                 }
             });
 
             return {
-                correct: correct,
-                num: num,
+                correct: this.where({correct: true}).length,
+                num: this.length,
                 answers: answers
             }
         }
@@ -95,34 +94,31 @@
                 this.$el.html(new QuestionView({model: model}).render().$el);
             } else {
                 var stats = this.collection.getStats();
-
                 this.$el.html(_.template(this.template, stats));
             }
-
             return this;
         }
     });
 
     function startQuiz() {
-
         var libs = [
-            {'name': "Bacon", 'isLib': true},
-            {'name': "Tiger", 'isLib': true},
+            {'name': "Bacon", 'isLib': true, 'link': 'http://baconjs.github.io/'},
+            {'name': "Tiger", 'isLib': true, 'link': 'https://github.com/doublerebel/tiger'},
             {'name': "Ham", 'isLib': false},
-            {'name': "Broccoli", 'isLib': true},
+            {'name': "Broccoli", 'isLib': true, 'link': 'https://github.com/joliss/broccoli'},
             {'name': "Smårettskinke", 'isLib': false},
-            {'name': "Pasta", 'isLib': true},
-            {'name': "Prawn", 'isLib': true},
-            {'name': "Diabetes", 'isLib': true},
-            {'name': "Sushi", 'isLib': true},
-            {'name': "Shakespeare", 'isLib': true}
+            {'name': "Pasta", 'isLib': true, 'link': 'https://github.com/ympbyc/Pasta'},
+            {'name': "Prawn", 'isLib': true, 'link': 'https://github.com/yob/prawn-js'},
+            {'name': "Diabetes", 'isLib': false},
+            {'name': "Sushi", 'isLib': true, 'link': 'https://github.com/inf0rmer/Sushi-JS'},
+            {'name': "Shakespeare", 'isLib': true, 'link': 'http://hackage.haskell.org/package/shakespeare-js-1.0.2'}
         ];
 
         var startDiv = $('#start_div').hide();
-        var quizView = new QuizView({collection: new Questions(shuffle(libs))}).render();
+        var quizView = new QuizView({
+            collection: new Questions(shuffle(libs))
+        }).render();
         startDiv.after(quizView.$el);
     }
-
     $('#start').click(startQuiz);
-
 }());
